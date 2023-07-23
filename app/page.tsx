c93090ec01d46a18500e8869b8c4d9e92d0d1efd
Joe Lee <111937630+j1yl@users.noreply.github.com>
2023-07-23 14:18:06 -0700

    Add user bar and helper functions (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import MazeNode from "./components/Node";
+import Bar from "./components/Bar";
 
 export default function Home() {
+  const [selectedAlgo, setSelectedAlgo] = useState<string>("");
+
+  function visualize() {
+    console.log("visualize", selectedAlgo);
+  }
+
+  function clearBoard() {
+    console.log("board is cleared");
+  }
+
   return (
     <main>
+      <Bar
+        setSelectedAlgo={setSelectedAlgo}
+        visualize={visualize}
+        clearBoard={clearBoard}
+      />
       <MazeNode x={0} y={0} />
     </main>
   );
